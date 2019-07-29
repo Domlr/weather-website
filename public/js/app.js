8dc8a47de8c6a4a515b2a354b43158a2c3dda32c
dominic Roof <dominicroof@dominics-MacBook-Pro.local>
2019-07-29 14:08:14 +0800

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const enter = document.querySelector('form').addEventListener('submit', (e)=>{
    message1.textContent="Weather Forecast Loading..."
    message2.textContent="";
     const location = search.value;
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    fetch(`/weather?address=${location}`).then((response)=>{
         response.json().then((data)=>{
         if(data.error){
             message1.textContent='';
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000;
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -94,5 +95,6 @@ app.get('*', (req, res)=>{
 
     })
 })
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
+    console.log('server is up on' + port)
 });
